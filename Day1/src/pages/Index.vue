b8f1c5f28bc449b3fa7ef1599b4d9b070af28c88
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-23 22:00:12 +0800

    simple form elements

--- Day1/src/pages/Index.vue
+++ Day1/src/pages/Index.vue
@@ -1,14 +1,37 @@
 <template>
   <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    <q-form>
+      <input class="" type="text" placeholder="Last Name"><br>
+      <input class="" type="text" placeholder="First Name"><br>
+      <input class="" type="text" placeholder="Middle Name"><br>
+      <input class="" type="date" placeholder="Birthday"><br>
+      <textarea class="" placeholder="Full Address" rows="5" cols="20"></textarea><br>
+      <input class="" type="radio" name="gender" value="male" checked>Male<br>
+      <input class="" type="radio" name="gender" value="female">Female<br>
+      <input class="" type="radio" name="gender" value="other">Other<br>
+      <select name="civil">
+        <option value="" selected>Civil Status</option>
+        <option value="single">Single</option>
+        <option value="married">Married</option>
+        <option value="widowed">Widowed</option>
+        <option value="divorce">Divorced</option>
+      </select>
+    </q-form>
   </q-page>
+
 </template>
 
 <style>
 </style>
 
 <script>
-export default {
-  name: 'PageIndex'
-}
+  export default {
+    name: 'PageIndex',
+    data () {
+      return {
+        date: '2019/02/01'
+      }
+    }
+  }
+
 </script>
