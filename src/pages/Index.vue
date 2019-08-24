88646e39fa505778262c088b3272728d2c0cc060
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-24 14:36:51 +0800

    added better validation and indication

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -93,25 +93,33 @@ export default {
       if(  this.lname <= 0 ){
         this.$q.notify({
           message: "First Name Required!",
-          timeOut: 2500
+          timeOut: 2500,
+          color: "red",
+          icon: "error"
         })
       }
       else if( this.fname.length <= 0 ){
         this.$q.notify({
           message: "Last Name Required!",
-          timeOut: 2500
+          timeOut: 2500,
+          color: "red",
+          icon: "error"
         })
       }
       else if( this.mname <= 0 ){
         this.$q.notify({
           message: "Middle Name Required!",
-          timeOut: 2500
+          timeOut: 2500,
+          color: "red",
+          icon: "error"
         })
       }
       else{
         this.$q.notify({
           message: "Hi " + this.fname + " " + this.mname + " " + this.lname,
-          timeOut: 2500
+          timeOut: 2500,
+          color: "green",
+          icon: "done"
         })
       }
     },
