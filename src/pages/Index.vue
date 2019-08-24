450c6447d7d329ec349e37203a9e638031876b9b
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-24 13:37:41 +0800

    updated responsiveness

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -23,19 +23,19 @@
         label="Full Address"
         v-model="fulladdress"
         type="textarea"
-        class="col-xs-12 col-md-6 col-lg-4 q-pa-md"
+        class="col-xs-12 col-md-12 col-lg-12 q-pa-md"
       />
       <!-- Last Name<input class="col-xs-12 q-mx-md" type="text" placeholder="Last Name" v-model="fname" name="Last Name" id="">
       First Name<input class="col-xs-12 q-mx-md" type="text" placeholder="First Name" name="First Name" id="">
       Middle Name<input class="col-xs-12 q-mx-md" type="text" placeholder="Middle Name" name="Middle Name" id="">
       Birthday<input class="col-xs-12 q-mx-md" type="date" placeholder="Birthday" name="Birthday" id="">
       Full Address<textarea class="col-xs-12 q-mx-md" placeholder="Full Address" name="Full Address" id="" rows="4" cols="50" /> -->
-      <div id="gender" class="col-xs-6">
+      <div id="gender" class="col-xs-12 col-md-6 col-lg-6 q-pa-md">
         Gender
         <q-radio v-model="gender" val="Male" label="Male" />
         <q-radio v-model="gender" val="Female" label="Female" />
       </div>
-      <div class="col-xs-12">
+      <div class="col-xs-12 col-md-12 col-lg-6 q-pa-md">
         Civil Status
         <q-select filled v-model="cStatus" :options="options" label="Filled" />
       </div>
@@ -44,17 +44,17 @@
         <q-input filled class="col-xs-12 col-md-6 col-lg-4 q-pa-md" v-model="fullname" standout="bg-red text-white" label="Name of your husband/wife" />
         <!-- <input type="text" v-model="fullname" name="" id=""> -->
       </div>
-      <div class="col-xs-12" v-if="cStatus == 'Single' ">
+      <div class="col-xs-12 col-md-12 col-lg-12 q-pa-md" v-if="cStatus == 'Single' ">
         Mag mingle kana!
         <!-- <input type="text" v-model="fullname" name="" id=""> -->
       </div>
       <div class="col-xs-12" v-for="(edu, index) in education" :key="index">
         <!-- <input type="text" v-model="edu.school" name="" id=""> -->
-        <q-input filled class="col-xs-12 col-md-6 col-lg-4 q-pa-md" v-model="edu.school" standout="bg-red text-white" label="Name of School" />
+        <q-input filled class="col-xs-12 col-md-6 col-lg-6 q-pa-md" v-model="edu.school" standout="bg-red text-white" label="Name of School" />
         <!-- <button @click="delSchool(index)">Delete School</button> -->
-        <q-btn color="red" @click="delSchool(index)" label="Delete School" />
+        <q-btn color="red" class="col-xs-12 col-md-3 col-lg-3 q-pa-md" @click="delSchool(index)" label="Delete School" />
       </div>
-        <q-btn color="primary" @click="addSchool" label="Add School" />
+      <q-btn color="primary" @click="addSchool" label="Add School" />
       <!-- <button @click="addSchool">Add new School</button> -->
     </div>
   </q-page>
