706dc03b22dbef98c2ac3facec8026c96267e0af
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-24 10:41:42 +0800

    added improved input form with sample

--- Day2/src/pages/Index.vue
+++ Day2/src/pages/Index.vue
@@ -1,29 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-form>
-      <label for="lastName">Last Name</label>
-      <input class="" type="text" name="lastName" placeholder="Last Name"><br>
-      <label for="firstName">First Name</label>
-      <input class="" type="text" name="firstName" placeholder="First Name"><br>
-      <label for="middleName">Middle Name</label>
-      <input class="" type="text" name="middleName" placeholder="Middle Name"><br>
-      <label for="birthday">Birthday</label>
-      <input class="" type="date" name="birthDay" placeholder="Birthday"><br>
-      <label for="fullAddress">Address</label>
-      <textarea class="" name="fullAddress" placeholder="Full Address" rows="5" cols="20"></textarea><br>
-      <label for="gender">Gender</label><br>
-      <input class="" type="radio" name="gender" value="male" checked>Male<br>
-      <input class="" type="radio" name="gender" value="female">Female<br>
-      <input class="" type="radio" name="gender" value="other">Other<br>
-      <label for="civil">Civil Status</label>
-      <select name="civil">
+  <q-page padding>
+    <div class="q-pa-md row container-fluid">
+      <!-- OUTPUT PORTION -->
+      <div class="container cols-xs-12">
+        <div class="row">
+          <p>First Name is : {{firstName}}</p>
+        </div>
+      </div>
+
+      Last Name <input class="col-xs-12 q-mx-md " type="text" name="lastName" placeholder="Last Name">
+      First Name <input class="col-xs-12 q-mx-md" type="text" name="firstName" placeholder="First Name">
+      Middle Name <input class="col-xs-12 q-mx-md" type="text" name="middleName" placeholder="Middle Name">
+      Birthday <input class="col-xs-12 q-mx-md" type="date" name="birthDay" placeholder="Birthday">
+      Address <textarea class="col-xs-12 q-mx-md" name="fullAddress" placeholder="Full Address" rows="5" cols="20"></textarea>
+      <div id="gender" class="col-xs-12">
+        Gender
+        <input class="" type="radio" name="gender" value="male" checked>Male
+        <input class="" type="radio" name="gender" value="female">Female
+        <input class="" type="radio" name="gender" value="other">Other
+      </div>
+
+      Civil Status
+      <select name="civil" class="col-xs-12 q-mx-md">
         <option value="" selected disabled>Civil Status</option>
         <option value="single">Single</option>
         <option value="married">Married</option>
         <option value="widowed">Widowed</option>
         <option value="divorce">Divorced</option>
       </select>
-    </q-form>
+    </div>
   </q-page>
 
 </template>
@@ -37,6 +42,11 @@
     data () {
       return {
         date: '2019/02/01'
+      }
+    },
+    data(){
+      return {
+        firstName:'Adrian'
       }
     }
   }
