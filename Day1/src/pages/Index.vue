b7100b3b527d58ce7361440348f57095b65a72e9
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-24 09:57:29 +0800

    day1 updated

--- Day1/src/pages/Index.vue
+++ Day1/src/pages/Index.vue
@@ -1,16 +1,23 @@
 <template>
   <q-page class="flex flex-center">
     <q-form>
-      <input class="" type="text" placeholder="Last Name"><br>
-      <input class="" type="text" placeholder="First Name"><br>
-      <input class="" type="text" placeholder="Middle Name"><br>
-      <input class="" type="date" placeholder="Birthday"><br>
-      <textarea class="" placeholder="Full Address" rows="5" cols="20"></textarea><br>
+      <label for="lastName">Last Name</label>
+      <input class="" type="text" name="lastName" placeholder="Last Name"><br>
+      <label for="firstName">First Name</label>
+      <input class="" type="text" name="firstName" placeholder="First Name"><br>
+      <label for="middleName">Middle Name</label>
+      <input class="" type="text" name="middleName" placeholder="Middle Name"><br>
+      <label for="birthday">Birthday</label>
+      <input class="" type="date" name="birthDay" placeholder="Birthday"><br>
+      <label for="fullAddress">Address</label>
+      <textarea class="" name="fullAddress" placeholder="Full Address" rows="5" cols="20"></textarea><br>
+      <label for="gender">Gender</label><br>
       <input class="" type="radio" name="gender" value="male" checked>Male<br>
       <input class="" type="radio" name="gender" value="female">Female<br>
       <input class="" type="radio" name="gender" value="other">Other<br>
+      <label for="civil">Civil Status</label>
       <select name="civil">
-        <option value="" selected>Civil Status</option>
+        <option value="" selected disabled>Civil Status</option>
         <option value="single">Single</option>
         <option value="married">Married</option>
         <option value="widowed">Widowed</option>
