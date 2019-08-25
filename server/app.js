838305689416fe4005a44cf0b3190b956716db6c
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-25 14:54:45 +0800

    improved routing

--- server/app.js
+++ server/app.js
@@ -1,13 +1,17 @@
 const express = require ('express')
-// var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
-// var url = "mongodb://localhost:27017/codestack";
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.use(bodyParser.json())
+
+var url = "mongodb://localhost:27017/codestack";
 
 // MongoClient.connect(url, function(err, db) {
 //   if (err) throw err;
@@ -15,57 +19,55 @@ app.use(bodyParser.json());
 //   db.close();
 // });
 
-
 var users = ['dave','john','doe']
-var musics = [
+var music = [
   {'name' : 'music1'},
   {'name' : 'music2'},
   {'name' : 'music3'},
   {'name' : 'music4'},
 ]
 
-// DATA GET
+/////////////////////////////////////////////////// DATA GET
 app.get('/', (req,res) => {
   res.send('Hello World!')
 })
 
 app.get('/users', ( req , res ) => {
-  res.json( {'users' : musics} )
+  res.json( {'users' : users} )
 })
+
 app.get('/users/:userid', ( req , res ) => {
   res.send(`Hello! ${req.params.userid} `)
 })
 
-app.get('/musics', ( req , res ) => {
-  res.json( {'musics' : musics} )
+app.get('/music', ( req , res ) => {
+  res.json( {'music' : music} )
 })
 
-app.get('/musics/:name', ( req , res ) => {
-  res.send(`Music ${req.params.name} is Great!`)
+app.get('/music/:id', ( req , res ) => {
+  res.json( music[req.params.id] )
 })
 
-// DATA POST
+/////////////////////////////////////////////////// DATA POST
 
 // ADD NEW MUSIC
 app.post('/music', ( req , res ) => {
-  musics.push(req.body)
-  res.json( { 'added' : req.body } )
+  music.push( req.body )
+  res.json( { 'added' : music.slice(-1)[0] } )
+})
+
+////////////////////////////////////////////////// DATA UPDATE
+
+// UPDATE A MUSIC
+app.put('/music/:id', ( req , res ) =>{
+  music[req.params.id] = req.body
+  res.json( { 'updated' : music[req.params.id] } )
 })
 
 
 
 
-
-function sendHello( req , res , next ){
-  if ( req.params.userid ){
-    next()
-  }
-  else{
-    res.send(`Parameter userid not supplied`)
-  }
-}
-
-
+///////////////////////////////////////////////////// START SERVER
 app.listen(port, () => {
   console.log(`Example app listend on port ${port}`)
 })
