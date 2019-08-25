92610b92a902bbbff5d88714a769e6f88873a258
=ad23 <adrian.cenizal@achievewithoutborders.org>
2019-08-25 13:27:59 +0800

    added additional routes

--- server/app.js
+++ server/app.js
@@ -3,6 +3,29 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req,res) => res.send('Hello World!'))
+app.get('/', (req,res) => {
+  res.send('Hello World!')
+})
 
-app.listen(port, () => console.log(`Example app listend on port ${port}`))
+app.get('/users/:userid', sendHello, ( req , res ) => {
+  res.send(`Hello! ${req.params.userid} `)
+})
+
+app.post('/music/:userid', sendHello, ( req , res ) => {
+  res.send(`Hello! ${req.params.userid} `)
+})
+
+
+function sendHello( req , res , next ){
+  if ( req.params.userid ){
+    next()
+  }
+  else{
+    res.send(`Parameter userid not supplied`)
+  }
+}
+
+
+app.listen(port, () => {
+  console.log(`Example app listend on port ${port}`)
+})
